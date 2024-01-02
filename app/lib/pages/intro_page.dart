ddefd92a939435db29509262e7a445c99c8cd21c
Muditha Pradeeptha <it21235924@my.sliit.lk>
2024-01-02 23:59:19 +0530

    second day

---
+++ app/lib/pages/intro_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+    );
+  }
+
+}
