8eb1a0f41d7285b0b3909a8c6d6a991213dff849
Muditha Pradeeptha <it21235924@my.sliit.lk>
2024-01-05 23:50:12 +0530

    3rd

--- app/lib/pages/intro_page.dart
+++ app/lib/pages/intro_page.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
 
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.red,
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 216, 75, 75),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 25),
+
+            //shop name
+            Text("SUSHI MANIA", style: GoogleFonts.dmSerifDisplay(),),
+
+            //icon
+
+            //subtitle
+
+            //get start button
+
+
+          ],
+        ),
+      ),
     );
   }
 
